0f2c1d15cb33ffed24b1e3ddb3209cb24ac1774c
phoeN24 <phoenix@kilodelta.com>
2022-01-25 19:44:24 -0800

    working commit 1

---
+++ solo/server/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.listen(3000);
---
+++ solo/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
+  document.getElementById('app'),
+)
